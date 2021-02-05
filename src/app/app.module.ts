178e0dbcbaf55806460d9dcab4d0ffcedf7f9da0
Olya <olyabusol1605@gmail.com>
2021-02-05 22:19:49 +0200

    AngularLesson5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {UsersComponent} from './component/users/users.component';
 import {Form1Component} from './component/form1/form1.component';
 import {Form2Component} from './component/form2/form2.component';
 import {RouterModule} from '@angular/router';
@@ -11,7 +10,7 @@ import {RouterModule} from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
+
     Form1Component,
     Form2Component
   ],
