735fcb5276ae665cf5d958c445e11d1521b61518
Olya <olyabusol1605@gmail.com>
2021-02-05 22:17:59 +0200

    AngularLesson5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {UsersComponent} from './component/users/users.component';
+import {Form1Component} from './component/form1/form1.component';
+import {Form2Component} from './component/form2/form2.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    Form1Component,
+    Form2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'form1', component: Form1Component
+      },
+      {
+        path: 'form2', component: Form2Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
